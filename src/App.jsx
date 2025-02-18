9c19f2fc88a3bf7137ae25a6a40f164f9459b8f1
Danielle Bassetto <danibbassetto@hotmail.com>
2025-02-17 22:04:45 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Welcome from './components/Welcome'
+import Question from './components/Question'
+import { QuizContext } from './context/quiz';
+import { useContext } from 'react';
+import './App.css'
+
+function App() {
+  const [quizState, dispatch] = useContext(QuizContext);
+
+  return (
+    <div className='App'>
+      <h1>Quiz de Programação</h1>
+      {quizState.gameStage === "Start" && <Welcome/>}
+      {quizState.gameStage === "Playing" && <Question/>}
+    </div>
+  )
+}
+
+export default App
